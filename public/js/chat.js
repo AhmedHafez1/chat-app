a3947ac5c5bb1a0ef6f9b7364c1ca7f786b5c6ab
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-12-01 14:34:50 +0200

    c

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,8 @@
-io();
+const socket = io();
+socket.on('countUpdated', (counter) => {
+  console.log('Counter updated : ', counter);
+});
+
+document.querySelector('#increment').addEventListener('click', () => {
+  socket.emit('increment');
+});
