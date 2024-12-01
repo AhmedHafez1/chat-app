9a4f5a30985e4604d93df439c9e9e63cac8eef73
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-12-01 07:42:56 +0200

    Added socket.io

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+const http = require('http');
 const express = require('express');
 const path = require('path');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port = process.env.PORT || 3000;
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+io.on('connection', () => {
+  console.log('New Web Socket Connection!');
+});
+
+server.listen(port, () => console.log(`Server listening on port ${port}!`));
