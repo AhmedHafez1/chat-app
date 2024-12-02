7ce54407173f50d8b2a5584cd0ea2e9bff6ad5c5
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-12-02 09:53:55 +0200

    A user has left!

--- src/index.js
+++ src/index.js
@@ -15,10 +15,16 @@ app.use(express.static(publicDirectoryPath));
 
 io.on('connection', (socket) => {
   console.log('New Web Socket Connection!');
+
   socket.emit('message', 'Welcome!');
+  socket.broadcast.emit('message', 'A new user has joined!');
 
   socket.on('newMessage', (message) => {
     io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left!');
   });
 });
 
