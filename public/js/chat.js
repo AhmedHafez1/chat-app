9b26c40ae3292c1fae244ed0da1b59535272eb1e
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2024-12-02 10:20:03 +0200

    sendLocation

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,3 +16,16 @@ document.querySelector('#chat').addEventListener('submit', function (e) {
 
   socket.emit('newMessage', input);
 });
+
+document.querySelector('#send-location').addEventListener('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser!');
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  });
+});
